df964fc9db37f025ad9600a6348954562daf1259
IgorAleks88 <igor.alexeyenko@gmail.com>
2020-10-12 10:16:52 +0300

    refactory: add solution for quantity of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsCount=0;
+  for (let i=0; i<bracketsConfig.length; i++){
+    let openBrackets=0;
+    let closeBrackets=0;
+    for (let j=0; j<str.length; j++) {
+      if (str[j]==bracketsConfig[i][0]) {openBrackets++;}
+      if (str[j]==bracketsConfig[i][1]&&closeBrackets<openBrackets) {closeBrackets++;}
+    }
+if (closeBrackets==openBrackets) {bracketsCount++}
+  }
+  if (bracketsCount==bracketsConfig.length) {return true} else {return false}
 }
